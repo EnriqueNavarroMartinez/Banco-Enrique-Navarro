08913dfe2c7c75dd61db33c6c7cc60034f6f9cca
EnriqueNavarroMartinez <147039822+EnriqueNavarroMartinez@users.noreply.github.com>
2025-01-04 16:39:04 +0100

    a

--- app/src/main/java/com/example/banco_ennama/activities/MainActivity.kt
+++ app/src/main/java/com/example/banco_ennama/activities/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.example.banco_ennama.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.banco_ennama.R
 import com.example.banco_ennama.databinding.ActivityMainBinding
+import com.example.bancoapiprofe.pojo.Cliente
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val cliente = intent.getSerializableExtra("Cliente") as Cliente
+        binding.tv2.text = cliente.getNombre()
+
         // Configuración del Toolbar
         setSupportActionBar(binding.appbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.bank_24) // Ícono de hamburguesa o cualquier ícono que quieras usar
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.bank_24)
 
         // Configuración del NavigationView
         binding.navigationView?.setNavigationItemSelectedListener(this)
@@ -34,13 +43,55 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.string.open_nav, R.string.close_nav
         )
         binding.drawerLayout?.addDrawerListener(toggle)
-        toggle.syncState() // Sincroniza el estado del Drawer con el botón del Toolbar
+        toggle.syncState()
+
+        // Configuración de los botones
+        binding.cambiarContrasena.setOnClickListener { navigateToPassword(cliente) }
+        binding.transferencias.setOnClickListener { navigateToTransfer(cliente) }
+        binding.salir.setOnClickListener { navigateToWelcome() }
+        binding.movimientos.setOnClickListener { navigateToMovements(cliente) }
+        binding.posicionGlobal.setOnClickListener { navigateToGlobalPosition(cliente) }
+
+        // Configuración para los insets de las barras del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun navigateToGlobalPosition(cliente: Cliente) {
+        val intent = Intent(this, GlobalPositionActivity::class.java)
+        intent.putExtra("Cliente", cliente)
+        startActivity(intent)
+    }
+
+    private fun navigateToMovements(cliente: Cliente) {
+        val intent = Intent(this, MovementsActivity::class.java)
+        intent.putExtra("Cliente", cliente)
+        startActivity(intent)
+    }
+
+    private fun navigateToTransfer(cliente: Cliente) {
+        val intent = Intent(this, TransferActivity::class.java)
+        intent.putExtra("Cliente", cliente)
+        startActivity(intent)
+    }
+
+    private fun navigateToPassword(cliente: Cliente) {
+        val intent = Intent(this, PasswordActivity::class.java)
+        intent.putExtra("Cliente", cliente)
+        startActivity(intent)
+    }
+
+    private fun navigateToWelcome() {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                // Si se presiona el ícono de hamburguesa, abrir el Drawer
                 binding.drawerLayout?.openDrawer(GravityCompat.START)
                 true
             }
@@ -49,41 +100,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val cliente = intent.getSerializableExtra("Cliente") as Cliente
         when (item.itemId) {
-            R.id.nav_global_position -> {
-                // Lógica para manejar el ítem "Posición Global"
-                val intent = Intent(this, GlobalPositionActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_movements -> {
-                // Lógica para manejar el ítem "Movimientos"
-                val intent = Intent(this, MovementsActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_transfers -> {
-                // Lógica para manejar el ítem "Transferencias"
-                val intent = Intent(this, TransferActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_change_password -> {
-                // Lógica para manejar el ítem "Cambiar Contraseña"
-                val intent = Intent(this, PasswordActivity::class.java)
-                startActivity(intent)
-            }
+            R.id.nav_global_position -> navigateToGlobalPosition(cliente)
+            R.id.nav_movements -> navigateToMovements(cliente)
+            R.id.nav_transfers -> navigateToTransfer(cliente)
+            R.id.nav_change_password -> navigateToPassword(cliente)
             R.id.nav_promotions -> {
-                // Lógica para manejar el ítem "Promociones"
+                // Lógica para promociones
             }
             R.id.nav_atms -> {
-                // Lógica para manejar el ítem "Cajeros"
+                // Lógica para cajeros
             }
-            R.id.nav_exit -> {
-                // Lógica para manejar el ítem "Salir"
-                val intent = Intent(this, WelcomeActivity::class.java)
-                startActivity(intent)
+            R.id.nav_sett -> {
+                // Lógica para settings
             }
+            R.id.nav_exit -> navigateToWelcome()
         }
-        // Cerrar el Drawer después de seleccionar una opción
         binding.drawerLayout?.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun enableEdgeToEdge() {
+
     }
 }
